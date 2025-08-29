8b4a049a5f36c1786ca059f3c32b2669216795c3
Edima <edimaudo@gmail.com>
2025-08-29 18:46:11 +0000

    update

---
+++ Neighborhood_Risk_Predictor.py
@@ -0,0 +1,78 @@
+from utils import *
+from data import *
+import config
+#from config import *
+
+st.title(APP_NAME)
+st.header(PREDICTON_HEADER)
+
+with st.sidebar:
+    month_options = st.selectbox('Month',MONTH)
+    dow_options = st.selectbox('Day of Week',DAY_OF_WEEK)
+    hour_options = st.selectbox('Hour',HOUR)
+    premises_options = st.selectbox('Premises Type',PREMISES_TYPE)
+    neighbourhood_options = st.selectbox('Neighborhood',NEIGHBORHOOD)
+    clicked = st.button("Run Prediction")
+
+# Model information
+model_data = df_filtered[['OCC_MONTH','OCC_DOW','OCC_HOUR','PREMISES_TYPE','Neighborhood','MCI_CATEGORY']]
+# Categorize data
+model_data["OCC_MONTH"] = model_data["OCC_MONTH"].astype('category')
+model_data["OCC_DOW"] = model_data["OCC_DOW"].astype('category')
+model_data["PREMISES_TYPE"] = model_data["PREMISES_TYPE"].astype('category')
+model_data["Neighborhood"] = model_data["Neighborhood"].astype('category')
+model_data["MCI_CATEGORY"] = model_data["MCI_CATEGORY"].astype('category')
+model_data["OCC_MONTH_cat"] = model_data["OCC_MONTH"].cat.codes
+model_data["OCC_DOW_cat"] = model_data["OCC_DOW"].cat.codes
+model_data["PREMISES_TYPE_cat"] = model_data["PREMISES_TYPE"].cat.codes
+model_data["Neighborhood_cat"] = model_data["Neighborhood"].cat.codes
+model_data["MCI_CATEGORY_cat"] = model_data["MCI_CATEGORY"].cat.codes
+
+model_info = model_data[(model_data.OCC_MONTH == month_options)]
+model_info.reset_index(drop=True, inplace=True)
+month = model_info['OCC_MONTH_cat'][0]
+
+model_info = model_data[(model_data.OCC_DOW == dow_options)]
+model_info.reset_index(drop=True, inplace=True)
+dow = model_info['OCC_DOW_cat'][0]
+
+model_info = model_data[(model_data.OCC_HOUR == hour_options)]
+model_info.reset_index(drop=True, inplace=True)
+hour = model_info['OCC_HOUR'][0]
+
+model_info = model_data[(model_data.PREMISES_TYPE == premises_options)]
+model_info.reset_index(drop=True, inplace=True)
+premise = model_info['PREMISES_TYPE_cat'][0]
+
+model_info = model_data[(model_data.Neighborhood == neighbourhood_options)]
+model_info.reset_index(drop=True, inplace=True)
+neighbourhood = model_info['Neighborhood_cat'][0]
+
+if clicked:
+    info_df = pd.DataFrame(columns = ['OCC_MONTH_cat','OCC_DOW_cat','OCC_HOUR','PREMISES_TYPE_cat','Neighborhood_cat'],index = ['a'])
+    info_df.loc['a'] = [month,dow,hour, premise, neighbourhood]
+    # Load model
+    saved_final_model = load_model('model/Final Model')
+    # Prediction
+    new_prediction = predict_model(saved_final_model, data=info_df)
+    crime_category = new_prediction['prediction_label'][0]
+    if crime_category == 0:
+        crime_output = 'Assault'
+    elif crime_category == 1:
+        crime_output = 'Auto Theft'
+    elif crime_category == 2:
+        crime_output = 'Break & Enter'
+    elif crime_category == 3:
+        crime_output = 'Robbery'
+    else:
+        crime_output = 'Theft Over'
+    st.write("The Predicted Crime Category is: " + crime_output)
+    
+    st.subheader("Community Action Steps")
+    
+    st.write("Based on the crime category: " + crime_output + ', here are some safety recommendations for the community')
+    prompt = " You are a community safety advisor. Based on the following crime" + str(crime_output) + " that occurred in " + str(premises_options) + " at " + str(hour_options) + " hours in " + str(neighbourhood_options) + " a neigbhorhood in Toronto, Ontario, " + "generate 3 practical safety recommendations for local residents."
+    
+    config.model.generate(prompt)
+    prompt_output = config.model.generate_text(prompt)
+    outcome_txt = st.text_area(label=" ",value=prompt_output,placeholder='')
